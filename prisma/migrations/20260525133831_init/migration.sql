28d17d793d9d77940c557e625dd441cc83a4de50
mehdidelghavi <mehdidelghavideveloper@gmail.com>
2026-05-30 15:47:10 +0330

    Fix Code

--- prisma/migrations/20260525133831_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `servers` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `settings` JSON NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20260530120350_initial/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `servers` ALTER COLUMN `updatedAt` DROP DEFAULT;
